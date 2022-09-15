0567db31a530c1e66e52f8eed0fd961812978f49
Sean <12708@coderacademy.edu.au>
2022-09-15 16:58:22 +1000

    filter/search function built

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import numpy as n
 
 #welcome message when app opens:
 print('Welcome to FilmSpot: home to the top 1000 movies')
-
+# filter_method = str(input(f"Select how you want to filter our database [Name, Year, Genre, Cast, Director]: ").title())
 
 
 class App_dataframe:
@@ -18,48 +18,60 @@ class App_dataframe:
 
     #this is the method for the filter functionality
     @staticmethod
-    def filter_function(filter_method):
+    def filter_function():
 
 
-        # try:
-        if filter_method == "Name":
-            title_search = str(input(f'Enter your movie title: ').title())
-            result = app_df.loc[app_df['Name'] == (f'{title_search}')]
-            print(f'Yep! its in there: ')
-            print(result)
-        elif filter_method == "Year":
-            year_search = str(input(f'Enter a year: '))
-            result = app_df.loc[app_df['Year'] == (f'{year_search}')]
-            print(f'---Results for {year_search}---')
-            print(result)
-        elif filter_method == "Genre":
-            genre_search = (input(f'Enter one or more genres from the following [Action, Drama, Comedy, Adventure, Animation, Biography, Horror, Fantasy, History]: ').title())
-            # result = app_df.loc[app_df['Genre'] == (f'{genre_search}'.capitalize())]
-            result = app_df[app_df.isin([f'{genre_search}']).any(axis=1)]
-            if app_df.empty == False:
-                print(f'Results for {genre_search}: ')
-                print(result)
-            else:
-                
-        elif filter_method == "Cast":
-            cast_search = (input(f'Enter the name of an actor/acress: ').title())
-            result = app_df[app_df.isin([f'{cast_search}']).any(axis=1)]
-            print(f'Results for {cast_search}: ')
-            print(result)
-        if filter_method == "Director":
+        while True:
+            filter_method = str(input(f"Select how you want to filter our database [Name, Year, Genre, Cast, Director]: ").title())
+            while filter_method == "Name":
+                title_search = str(input(f'Enter your movie title: ').title())
+                result = app_df.loc[app_df['Name'] == (f'{title_search}')]
+                if title_search not in app_df.values:
+                    print('Hmm...Looks like that one is not in our top 1000. Check your spelling and try again')
+                    break
+                else:
+                    print(f'Yep! its in there: ')
+                    print(result)
+                    break
+
+            while filter_method == "Year":
+                year_search = str(input(f'Enter a year: '))
+                result = app_df.loc[app_df['Year'] == (f'{year_search}')]
+                if year_search not in app_df.values:
+                    print("Invalid input, give it another go")
+                else:
+                    print(f'---Results for {year_search}---')
+                    print(result)
+            while filter_method == "Genre":
+                genre_search = (input(f'Enter one or more genres from the following [Action, Drama, Comedy, Adventure, Animation, Biography, Horror, Fantasy, History]: ').title())
+                result = app_df[app_df.isin([f'{genre_search}']).any(axis=1)]
+                if genre_search not in app_df.values:
+                    print('Oops...Looks like what you have entered is invalid')
+                else:
+                    print(f'---Results for {genre_search}--- ')
+                    print(result)
+            while filter_method == "Cast":
+                cast_search = (input(f'Enter the name of an actor or actress: ').title())
+                result = app_df[app_df.isin([f'{cast_search}']).any(axis=1)]
+                if cast_search not in app_df.values:
+                    print(f"Hmm... either {cast_search} isnt in our 1000, or you made them up...")
+                else:
+                    print(f'---Results for {cast_search}--- ')
+                    print(result)
+            while filter_method == "Director":
                 director_search = str(input(f'Enter the full name of a director: ').title())
                 result = app_df.loc[app_df['Director'] == (f'{director_search}')]
-                print(f'Results for {director_search}: ')
-                print(result)
-            # if director_search not in app_df.values:
-        if app_df.empty == True:
-            print(f"Can't find that one, lets try again")
-            input((f'Enter the full name of a director: ').title())
+                if director_search not in app_df.values:
+                    print(f"Hmm... either {director_search} isnt in our 1000, or you made them up...")
+                else: 
+                    print(f'---Results for {director_search}--- ')
+                    print(result)
+            if filter_method == "Menu":
+                break
+            else:
+                print('Invalid input, give it another go')
 
-        # except [TypeError, IndexError]:
-        #     return "Invalid input"
+App_dataframe.filter_function()
 
-filter_method = str(input(f"Select how you want to filter our database [Name, Year, Genre, Cast, Director]: ").title())
-
-# App_dataframe.filter_function(filter_method)
+print("Welcome back to the main menu")
 
