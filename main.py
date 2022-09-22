9e8b16939150987982669103c2576088181b8d3a
Sean <12708@coderacademy.edu.au>
2022-09-22 16:45:21 +1000

    error exception added for keyboard interrupt

--- main.py
+++ main.py
@@ -8,15 +8,16 @@ from AppDf import *
 
 if __name__ == '__main__':
     #welcome message when app opens:
+    try:
+        print('Welcome to FilmSpot: home to the top 1000 movies\n -----------------------------------------------------')
 
-    print('Welcome to FilmSpot: home to the top 1000 movies\n -----------------------------------------------------')
-
-    print('*** Always remember, to go back, just enter: back ****\n')
+        print('*** Always remember, to go back, just enter: back ****\n')
 
 
 
-    # this triggers the main menu upon start up
-    App_dataframe.show_main_menu()
-
+        # this triggers the main menu upon start up
+        App_dataframe.show_main_menu()
+    except (KeyboardInterrupt):
+        print("\n\nSad to see you go. Until next time!")
 
 
